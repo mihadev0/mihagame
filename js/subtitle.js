c8a419b3b431d703796e68b18431a0b614c807bc
mihadev0 <117792648+mihadev0@users.noreply.github.com>
2023-11-04 12:23:02 -0700

    Add files via upload

---
+++ js/subtitle.js
@@ -0,0 +1,142 @@
+const say = [
+  "The tech teams worst nightmare.",
+  "Better than coolmathgames",
+  "HEY! is this thing on",
+  "Don't get caught",
+  "There is definitely no games here",
+  "ONG",
+  "🤨",
+  "This project was made possible due to extensive use of narcotics",
+  "Last chance to look at me Hector",
+  "👋hello👋 and welcome to the Los Pollos hermanos 👪family👪👪 my name📛 is Gustavo but you can 📲call 📲 me Gus",
+  "According to all known laws of aviation,  : there is no way a bee should be able to fly.",
+  "amojngus ahdahahahayah i scab cab  cab can spell correctky ahahahha your bad and im gododd",
+  "no school, no need to block this",
+  "I made it mom! I'm on a randomized subtitle!",
+  "Playing games in my class, ay?",
+  "A moment of silence for adobe flash player",
+  "Whats my name? You're Heisenberg. You're God Damn Right",
+  "Anyone remember when the background was white? No? Just me?",
+  "If you are over 25 and own a computer, you wont last 5 seconds playing these games",
+  "only in ohio 💀",
+  "The Rizzard of Oz",
+  "We have a <a href='https://discord.com/invite/Qf5wmbGzk9'>Discord</a> Now",
+  "the god made this site",
+  "good soup 🍲",
+  "You wouldn't download a car?",
+  "L for Coolmath. This site is better still.",
+  "Jessie you were supposed to get a plastic container",
+  "N Gon go hard on this site",
+  "This is just schoology ok? idk or what i think this is a glitch",
+  "oooohhhh mario's tunnel of doom very scary",
+  "goofy ahh no admins",
+  "(Hello this is the principal of Elmer High, please stop playing games and take down this site.)-Some Bum Loser Probably",
+  "We should re-name the website to yourmom.com",
+  "Ur moms favorite ",
+  "I'm Gustavo But you can call me Gus",
+  "Walter Black",
+  "Does a set of all sets contain itself?",
+  " i died on my 12 year hardcore minecraft world  :(",
+  "Ngl CFISD Goofy ASF",
+  "the fitnessgram pacer test is a multistage aerobic capacity test",
+  "i know for a fact that you have a 60 in math and science and you playin games you bozo",
+  "Do your balls itch?",
+  "These aren't the games you're looking for",
+  "Nefarious Darius",
+  "numnut",
+  "👹I like to creep around my home and act like a goblin👹",
+  "Among Us VR?",
+  "If you ain’t in the <a href='https://discord.com/invite/Qf5wmbGzk9'>discord server</a> then where the hell you atttt", 
+  "<img src='/img/bingchilling.jpg'/>",
+  "i lost my v card two days ago thanks to this site",
+  "error = ran out of subtitles",
+  "your either a smart fella or a fart smella",
+  "Welcome to your final test, I’m Mr. Beast",
+  "better call saul",
+  "internet admin thinks they can stop us",
+  "Schoology is Down",
+  "I used this while trying to ignore the blood on the floor in school.",
+  "Where is the download ram button?",
+  "you know Pizza Hut, Just Do It",
+  "the last one to leave this website wins $100,000 - mr beast",
+  "Jesse, we need to game Jesse.",
+  "metro boomin make it boom ",
+  'as  the big man said "cock and balls"',
+  "what rhymes with digger",
+  "4 big guys:)",
+  "i ate the f***ing wii",
+  "it's really your own fault if you get caught",
+  "We are Number One",
+  "Abana",
+  "Click Me!",
+  "6353 Juan Tabo Apartment 6",
+  "Kanye did nothing wrong",
+  "Hey you, shut up",
+  "No maidens??",
+  "You need to leave or I wont leave your mom (this goes out to all the teachers)",
+  "Hey, it says gullible on the ceiling.",
+  "She doesn’t like you…",
+  " 🍑 i like big butts and i cannot lie  🍑",
+  "this website is the whole reason i exist",
+  "sometimes i sit and think about why we exist; the answer is.. this website",
+  "the best corn site everywhere",
+  "Connection terminated. I'm sorry to interrupt you, Elizabeth, if you still even remember that name, But I'm afraid you've been misinformed. You are not here to receive a gift, nor have you been called here by the individual you assume, although, you have indeed been called. You have all been called here, into a labyrinth of sounds and smells, misdirection and misfortune. A labyrinth with no exit, a maze with no prize. You don't even realize that you are trapped. Your lust for blood has driven you in endless circles, chasing the cries of children in some unseen chamber, always seeming so near, yet somehow out of reach, but you will never find them. None of you will. This is where your story ends. And to you, my brave volunteer, who somehow found this job listing not intended for you, although there was a way out planned for you, I have a feeling that's not what you want. I have a feeling that you are right where you want to be. I am remaining as well. I am nearby. This place will not be remembered, and the memory of everything that started this can finally begin to fade away. As the agony of every tragedy should. And to you monsters trapped in the corridors, be still and give up your spirits. They don't belong to you. For most of you, I believe there is peace and perhaps more waiting for you after the smoke clears. Although, for one of you, the darkest pit of Hell has opened to swallow you whole, so don't keep the devil waiting, old friend. My daughter, if you can hear me, I knew you would return as well. It's in your nature to protect the innocent. I'm sorry that on that day, the day you were shut out and left to die, no one was there to lift you up into their arms the way you lifted others into yours, and then, what became of you. I should have known you wouldn't be content to disappear, not my daughter. I couldn't save you then, so let me save you now. It's time to rest - for you, and for those you have carried in your arms. This ends for all of us. End communication.",
+  "You don't exist",
+  "Yo Mister White I made us some games",
+  "ey bruh why you aint accept my friend request bruh",
+  "Yo Mister White I made us some games",
+  "Oh Romeo, Oh Romeo, why hath you blocked access to the original?",
+  "your teacher is right BEHIND YOU!",
+  "🍆💦",
+  "Aubin Barcheck- she'll see it",
+  "I got married thanks to this site",
+  "I got laid thanks",
+  "(this is the principal from forest hills high school please take down this site or we will sue) - some other loser",
+  "Fulcurm Jr.",
+  "<a href='https://pajort.org/'>Pajorts</a>, Put Your Ass In A Pair",
+  "i pit the new forgis on the jeep",
+  "THE CASTLE WAS HERE.",
+  '"To win the game, you must kill me, John Romero."',
+  "IDDQD - God Mode.",
+  "1, 2 buckle my shoe",
+  "i had sex ed today",
+  "Warning: Goofy chat can cause cancer",
+];
+
+var seCode = ['KeyA', 'KeyX', 'KeyE', 'KeyL'];
+var seCodePosition = 0;
+var vid = 'video/content.mp4';
+
+document.addEventListener('keydown', function(event) {
+  if (event.code === seCode[seCodePosition]) {
+    seCodePosition++;
+    if (seCodePosition === seCode.length) {
+      document.getElementById('subtitle').innerHTML = '<video  src="' + vid + '" width="77" height="43" autoplay loop></video>';
+      seCodePosition = 0;
+    }
+  } else {
+    seCodePosition = 0;
+  }
+});
+
+
+
+const changeSub = (num) => {
+  document.getElementById("subtitle").innerHTML = say[num];
+};
+
+const newSub = () => {
+  const howmany = say.length;
+  const bRand = Math.floor(Math.random() * howmany);
+  const sayWhat = say[bRand];
+  document.getElementById("subtitle").innerHTML = sayWhat;
+};
+
+newSub();
+
+const changeSplash = (num) => {
+  const subtitle = say[num];
+  document.getElementById("subtitle").innerHTML = subtitle;
+  const ret = `Set current splash to splash ${num}, ${subtitle}`;
+  return ret;
+};
